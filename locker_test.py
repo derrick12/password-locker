dc43ebd48037af0b236415b6e7bfab25d6ca0ab4
derrick12 <derrickwaweru@gmail.com>
2018-09-29 12:38:28 +0300

    create a test for multiple users

--- locker_test.py
+++ locker_test.py
@@ -41,6 +41,15 @@ class TestUser(unittest.TestCase,User):
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
 
+    def test_save_multiple_user(self):
+        '''
+    test_save_multiple_user to check if we can save multiple user objects to our user_list 
+    '''
+
+        self.new_user.save_user()
+        test_user = User("Test","user","0712345678","test@user.com") # the new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
 
 
 if __name__ == '__main__':

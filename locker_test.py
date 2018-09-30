9fbad4ffcf39ac17d7268a303dc12defa97c6eac
derrick12 <derrickwaweru@gmail.com>
2018-09-30 18:43:08 +0300

    add a test for saving multiple accounts

--- locker_test.py
+++ locker_test.py
@@ -72,6 +72,20 @@ class TestUser(unittest.TestCase,User,Credentials):
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 
+    def test_save_multiple_account(self):
+        '''
+        test_save_multiple_account to check if we can save multiple account objects to our account_list 
+        '''
+
+        self.new_account.save_account()
+        test_account = Credentials("Test","account12") # the new account
+        test_account.save_account()
+        self.assertEqual(len(Credentials.account_list),2)
+
+
+
+
+
     def test_delete_user(self):
         '''
         test_delete_user to test if we can remove a user from our user list 
